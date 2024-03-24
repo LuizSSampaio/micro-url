f8467a37a10d20a2f8d1474ef3f0a7064cab5399
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2024-03-24 08:13:51 -0300

    Added basic redirect implementation

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+#[derive(Debug)]
+enum SolveError {
+    UrlNotFound,
+}
+
+#[get("/{url_id}")]
+async fn redirect(path: web::Path<String>) -> impl Responder {
+    match solve_id(path.into_inner()).await {
+        Ok(url) => HttpResponse::Found().append_header(("location", url)).finish(),
+        Err(_) => HttpResponse::NotFound().finish(),
+    }
+}
+
+async fn solve_id(url_id: String) -> Result<String, SolveError> {
+    if url_id == "ffff" {
+        return Ok("https://youtube.com".to_string())
+    }
+    Err(SolveError::UrlNotFound)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(redirect)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
